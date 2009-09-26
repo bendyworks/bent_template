a49a717874447f32737733485d5bc38cc03e63e1
bendycode <stephen@bendycode.com>
2009-09-26 18:46:01 -0500

    removed db-populate in favor of Rails 2.3.4 seeds

--- bent_template.rb
+++ bent_template.rb
@@ -41,7 +41,6 @@ plugin 'cucumber', :git => 'git://github.com/aslakhellesoy/cucumber.git', :submo
 plugin 'webrat', :git => 'git://github.com/brynary/webrat.git', :submodule => true
 plugin 'paperclip', :git => 'git://github.com/thoughtbot/paperclip.git', :submodule => true
 plugin 'hoptoad', :git => 'git://github.com/thoughtbot/hoptoad_notifier.git', :submodule => true
-plugin 'db-populate', :git => 'git://github.com/ffmike/db-populate.git', :submodule => true
 plugin 'annotate_models', :git => 'git://github.com/bendycode/annotate_models', :submodule => true
 plugin 'html_matchers', :git => 'git://github.com/bendycode/html_matchers', :submodule => true
 
@@ -58,7 +57,7 @@ gem 'flog', :env => 'test'
 gem 'flay', :env => 'test'
 gem 'relevance-rcov', :env => 'test', :lib => 'rcov', :source => 'http://gems.github.com'
 gem 'remarkable_rails', :env => 'test'
-# gem 'launchy', :env => 'test' # have not used yet. plan to try
+gem 'launchy', :env => 'test'
 
 # TODO: fix tarantula setup. this is broken
 # gem 'relevance-tarantula', :env => 'test', :lib => 'relevance/tarantula', :source => 'http://gems.github.com'
@@ -84,9 +83,6 @@ run 'curl -L http://jqueryjs.googlecode.com/svn/trunk/plugins/form/jquery.form.j
 # generate authlogic
 # create seed data for an admin user
 # ensure "rake db:test:prepare" uses seed data
-
-# DB-POPULATE
-mkdir 'db/populate'
 
 bent_file 'config/database.yml.template'
 
